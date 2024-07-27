e84af0e4a2db2fb279f96cf6e5288064363fb8c4
Akhmad Salafudin <axmad386@gmail.com>
2024-07-28 00:01:01 +0700

    feat(blog_posts): adding published_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_26_163844) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_27_161811) do
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
