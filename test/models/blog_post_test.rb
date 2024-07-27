f7c40c0c32d6945852490ec202c8751cf446e1c3
Akhmad Salafudin <axmad386@gmail.com>
2024-07-28 00:28:15 +0700

    test(blog_posts): published, draft and scheduled scope

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? return true for draft blog post" do
+    assert blog_posts(:draft).draft?
+  end
+
+  test "draft? return false for published blog post" do
+    refute blog_posts(:published).draft?
+  end
+
+  test "scheduled? return true for scheduled blog post" do
+    assert blog_posts(:scheduled).scheduled?
+  end
 end
